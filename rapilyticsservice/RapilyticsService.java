528142d84d273b7cf530c2de163a37449e71afec
jamesdeancalhoun <jamesdeancalhoun@outlook.com>
2019-10-26 17:12:37 -0500

    Setting up http server for root

--- rapilyticsservice/RapilyticsService.java
+++ rapilyticsservice/RapilyticsService.java
@@ -1,5 +1,25 @@
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class RapilyticsService {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // Source material for setting up Java HTTP server
+        // https://www.codeproject.com/tips/1040097/create-a-simple-web-server-in-java-http-server
+        int port = 80;
+        try {
+            System.out.println("Starting server at Port " + port);
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            System.out.println("Hello World at Port " + port);
+            server.createContext("/", new RootHandler());
+            server.setExecutor(null);
+            server.start();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.toString());
+        }
     }
 }
---
+++ rapilyticsservice/RootHandler.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.*;
+
+public class RootHandler implements HttpHandler {
+
+    // @Override
+    public void handle(HttpExchange he) throws IOException {
+        int port = 80;
+        String response = "<h1>Hello World</h1>" + "<h1>Port: " + port + "</h1>";
+        he.sendResponseHeaders(200, response.length());
+        OutputStream os = he.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
