3fc0060708f205282464a4a2b7f5b9cac7f7abb4
jamesdeancalhoun <jamesdeancalhoun@outlook.com>
2019-10-27 10:49:27 -0500

    add prebuilt files

---
+++ asclepius_frontend/build/precache-manifest.a064e3d20312a71d4121edd2ef427506.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf89d39b06252f2211faf2ed02df10f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3901dc08a3d5decb57b7",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "45b977c6f0906af1607c",
+    "url": "/static/js/2.e2857019.chunk.js"
+  },
+  {
+    "revision": "3901dc08a3d5decb57b7",
+    "url": "/static/js/main.bd866c50.chunk.js"
+  },
+  {
+    "revision": "986b277da8cfcc2edd6c",
+    "url": "/static/js/runtime-main.c60fa28a.js"
+  }
+]);
